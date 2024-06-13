5238a78bd38685dd8fa33a1c103a6ac802cdc9c1
vladkuzhyr <vkuzir7@gmail.com>
2024-06-13 21:46:03 +0300

    init a project

---
+++ src/main/java/com/vlad/circledevs_test/CircleDevsTestApplication.java
@@ -0,0 +1,13 @@
+package com.vlad.circledevs_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleDevsTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CircleDevsTestApplication.class, args);
+  }
+
+}
